9d6c2d2e7115087b2f23b50f41f02510a545013f
“Lioved” <lioved.3801911642@ucaldas.edu.co>
2022-04-05 21:21:39 -0500

    segundo

--- src/main/java/edu/ucaldas/gps/GpsApplication.java
+++ src/main/java/edu/ucaldas/gps/GpsApplication.java
@@ -2,12 +2,24 @@ package edu.ucaldas.gps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GpsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GpsApplication.class, args);
+	}
+	
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "Lioved") String name) {
+	return String.format("Hello %s!", name);
 	}
 
 }
